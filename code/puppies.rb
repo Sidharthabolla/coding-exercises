56c89942f018e6556eff8864a1481321d0eedf0b
Sidhartha Bolla <sidhartha.bolla@gmail.com>
2021-08-19 17:03:01 -0500

    HTML Scraper: accesing paws.html page, grabbing navigation files, iterating through each file and grabbing elements with 'female-baby' text and returning the hrefs

--- code/puppies.rb
+++ code/puppies.rb
@@ -5,9 +5,32 @@
 #
 # Hint: You will likely want to use an HTML parser like Nokogiri and use the page's pagination to iterate each page.
 #
+require 'nokogiri'
+
 class Puppies
   DIRECTORY = ::File.join(::File.dirname(__FILE__), '../data')
 
   def self.parse
+    puppies_path = []
+    files_arr = []
+
+    # getting all href files in pagination
+    document = Nokogiri::HTML(File.read(DIRECTORY+"/paws.html"))
+    document.css('.pagination .page-link').each do |d|
+      files_arr.push(d.attributes["href"].value)
+    end
+
+    # removing dupicates
+    files = files_arr.uniq.reject { |file| file == '#' }
+
+    # searching through all the files and pushing to array
+    files.each do |file|
+      document = Nokogiri::HTML(File.read(DIRECTORY+"/"+file))
+      document.search('a:contains("Female - Baby")').each do |doc|
+        puppies_path.push(doc.attributes["href"].value)
+      end
+    end
+
+    return puppies_path
   end
 end
