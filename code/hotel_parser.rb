755258918a43baf44d35899b05b4f135f294ad4a
Sidhartha Bolla <sidhartha.bolla@gmail.com>
2021-08-19 23:35:40 -0500

    Hotel Parser: using struct to have hash and attribues, creating 3 struct: main struct that contains hotels struct and hotel struct to contain all hashs of room.

--- code/hotel_parser.rb
+++ code/hotel_parser.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class HotelParser
   # Implement a method to parse the contents of a YAML file and return
   # an object whose values are accessible using the [] operator or method calls
@@ -5,5 +7,36 @@ class HotelParser
   # Note: Use of the YAML library is fine (encouraged, even) but please don't
   #       use any third-party gems that enable the required functionality.
   def self.parse(filename)
+    hotels_hash = YAML.load_file(filename)
+    struct = Struct.new(:hotels)
+    return struct.new(get_hotels(hotels_hash["hotels"]))
+  end
+
+  private
+
+  #recursive call to get all hotels
+  def self.get_hotels(hotels_hash)
+    arr = []
+    hotel_struct = Struct.new(:name,:rooms)
+
+    hotels_hash.each do |hotel|
+      new_hotel = hotel_struct.new(hotel['name'],get_rooms(hotel['rooms']))
+      arr.push(new_hotel)
+    end
+
+    return arr
+  end
+
+  #recursive call to get all rooms
+  def self.get_rooms(rooms_hash)
+    arr = []
+    room_struct = Struct.new(:name,:amenities)
+
+    rooms_hash.each do |room|
+      new_room = room_struct.new(room['name'],room['amenities'])
+      arr.push(new_room)
+    end
+
+    return arr
   end
 end
